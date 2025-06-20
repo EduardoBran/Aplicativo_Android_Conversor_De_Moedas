7676db5ab951289e429c448aa7a6de10b5a84c41
EduardoBran <eduardo.ads1814@gmail.com>
2025-06-20 11:00:03 -0300

    adicionando repositorio helper com arquivo de constantes

---
+++ app/src/main/java/com/luizeduardobrandao/conversordemoeda/helper/CurrencyConstants.kt
@@ -0,0 +1,12 @@
+package com.luizeduardobrandao.conversordemoeda.helper
+
+class CurrencyConstants private constructor(){
+
+    object RATES {
+        const val DOLAR = 0.20
+        const val EURO = 0.18
+        const val IENE = 27.50
+        const val PESO_ARGENTINO = 42.00
+    }
+
+}
